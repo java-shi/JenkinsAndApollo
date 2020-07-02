68dee8d729b5428c74d8b5be07b7fae1af418e7d
java-shi <631288464@qq.com>
2020-07-02 14:26:37 +0800

    Jenkins and Apollo 08

--- src/main/java/com/sfy/MyApp.java
+++ src/main/java/com/sfy/MyApp.java
@@ -3,6 +3,7 @@ package com.sfy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -10,7 +11,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @Author: Sfy
  * @Date: 2020/7/2 0002 9:21
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.sfy.mapper")
 public class MyApp extends SpringBootServletInitializer {
 
